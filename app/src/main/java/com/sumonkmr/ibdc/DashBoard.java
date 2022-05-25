536d1ba2b42aba82757fdb845d3b560b631b379b
sumonkmr <sumonkmr.dev@gmail.com>
2022-05-25 16:27:37 +0600

    Drawer Navigation Implemented!

--- app/src/main/java/com/sumonkmr/ibdc/DashBoard.java
+++ app/src/main/java/com/sumonkmr/ibdc/DashBoard.java
@@ -1,14 +1,79 @@
 package com.sumonkmr.ibdc;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class DashBoard extends AppCompatActivity {
 
+    com.google.android.material.navigation.NavigationView nav;
+    androidx.appcompat.widget.Toolbar toolbar;
+    ActionBarDrawerToggle toggle;
+    DrawerLayout drawerLayout;
+
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        nav = (NavigationView)findViewById(R.id.nav_menu);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.division:
+                        Toast.makeText(getApplicationContext(), "This is Division", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.district:
+                        Toast.makeText(getApplicationContext(), "This is District", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.upazila:
+                        Toast.makeText(getApplicationContext(), "This is Upazila", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.union:
+                        Toast.makeText(getApplicationContext(), "This is Union", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.blood:
+                        Toast.makeText(getApplicationContext(), "This is Blood", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+                }
+                return true;
+            }
+
+            });
+
+
     }
 }
