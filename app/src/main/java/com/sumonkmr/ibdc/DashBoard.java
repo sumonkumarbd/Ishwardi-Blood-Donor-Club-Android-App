c025e3d39187604bad8f642f7fd7f8df3551bca2
sumonkmr <sumonkmr.dev@gmail.com>
2022-05-28 15:34:17 +0600

    firebase sign-out is implemented!!

--- app/src/main/java/com/sumonkmr/ibdc/DashBoard.java
+++ app/src/main/java/com/sumonkmr/ibdc/DashBoard.java
@@ -12,15 +12,15 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import nl.psdcompany.duonavigationdrawer.views.DuoDrawerLayout;
 import nl.psdcompany.duonavigationdrawer.widgets.DuoDrawerToggle;
 
 public class DashBoard extends AppCompatActivity implements View.OnClickListener {
 
-    com.google.android.material.navigation.NavigationView nav;
-    androidx.appcompat.widget.Toolbar toolbar;
-    DuoDrawerToggle toggle;
     DuoDrawerLayout drawerLayout;
+    FirebaseAuth auth;
 
 
     @Override
@@ -28,105 +28,9 @@ public class DashBoard extends AppCompatActivity implements View.OnClickListener
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
 
-//        toolbar = (Toolbar)findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-////        nav = (NavigationView)findViewById(R.id.nav_menu);
-//        drawerLayout = (DuoDrawerLayout) findViewById(R.id.drawer);
-//
-//        toggle = new DuoDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
-//        drawerLayout.setDrawerListener(toggle);
-//        toggle.syncState();
-//
-//
-//        View contentView = drawerLayout.getContentView();
-//        View menuView = drawerLayout.getMenuView();
-//
-//        LinearLayout ll_Home = ((View) menuView).findViewById(R.id.ll_Home);
-//        LinearLayout ll_Profile = menuView.findViewById(R.id.ll_Profile);
-//        LinearLayout ll_Setting = menuView.findViewById(R.id.ll_Setting);
-//        LinearLayout ll_Share = menuView.findViewById(R.id.ll_Share);
-//        LinearLayout ll_Logout = menuView.findViewById(R.id.ll_Logout);
-//        LinearLayout ll_about = menuView.findViewById(R.id.ll_about_us);
-
-
-
-
-//        ll_Home.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(getApplicationContext(),DispalyRequestsActivity.class));
-//                drawerLayout.closeDrawer();
-//            }
-//        });
-//        ll_Profile.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
-//        ll_Setting.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
-//        ll_Share.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
-//        ll_about.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
-//        ll_Logout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
+        auth = FirebaseAuth.getInstance();
 
         init();
-
-//        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-//            @Override
-//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//                switch (item.getItemId()) {
-//                    case R.id.division:
-//                        Toast.makeText(getApplicationContext(), "This is Division", Toast.LENGTH_SHORT).show();
-//                        drawerLayout.closeDrawer(GravityCompat.START);
-//                        break;
-//
-//                    case R.id.district:
-//                        Toast.makeText(getApplicationContext(), "This is District", Toast.LENGTH_SHORT).show();
-//                        drawerLayout.closeDrawer(GravityCompat.START);
-//                        break;
-//
-//                    case R.id.upazila:
-//                        Toast.makeText(getApplicationContext(), "This is Upazila", Toast.LENGTH_SHORT).show();
-//                        drawerLayout.closeDrawer(GravityCompat.START);
-//                        break;
-//
-//                    case R.id.union:
-//                        Toast.makeText(getApplicationContext(), "This is Union", Toast.LENGTH_SHORT).show();
-//                        drawerLayout.closeDrawer(GravityCompat.START);
-//                        break;
-//
-//                    case R.id.blood:
-//                        Toast.makeText(getApplicationContext(), "This is Blood", Toast.LENGTH_SHORT).show();
-//                        drawerLayout.closeDrawer(GravityCompat.START);
-//                        break;
-//                }
-//                return true;
-//            }
-//
-//            });
-
-
     }
 
     private void init() {
@@ -185,7 +89,8 @@ public class DashBoard extends AppCompatActivity implements View.OnClickListener
                 break;
 
             case R.id.ll_Logout:
-                Toast.makeText(this, "Logout...", Toast.LENGTH_SHORT).show();
+                auth.signOut();
+                startActivity(new Intent(this,SplashScreen.class));
                 break;
 
         }
