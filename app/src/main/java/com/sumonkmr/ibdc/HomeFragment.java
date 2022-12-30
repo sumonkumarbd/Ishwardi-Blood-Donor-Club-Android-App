5b2914bcf1feb647076e3aea7f568dfdeb8c5af5
sumonkmr <sumonkmr.dev@gmail.com>
2022-12-30 21:28:17 +0600

    Home Animation is done!!

--- app/src/main/java/com/sumonkmr/ibdc/HomeFragment.java
+++ app/src/main/java/com/sumonkmr/ibdc/HomeFragment.java
@@ -6,27 +6,24 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
-import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import android.os.Handler;
-import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
-import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.FullScreenContentCallback;
 import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
@@ -43,36 +40,60 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.Random;
 
 
 public class HomeFragment extends Fragment {
 
-    RelativeLayout ins,benefit,be_donor,find_donors, about_us,rate_us;
+    RelativeLayout ins,benefit,find_donors, about_us,rate_us;
+    LinearLayout first_row,sec_row,third_row;
+    SwipeRefreshLayout reloadHome;
     ImageSlider image_slider;
-    Intent targetActivity;
-    // per app run-- do not show more than 3 fullscreen ad. [[Change it if your want]]
-    int fullScreenAdMaxShowCount = 3;
+    View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-//        getAdsFirebase();
+        view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        Init();
+        ImageSlider();
+        OnClicks();
+        Anim();
+        Reload();
 
+        return view;
+
+    }
+
+    private void Init() {
         ins = view.findViewById(R.id.ins);
         benefit = view.findViewById(R.id.benefit);
         find_donors = view.findViewById(R.id.find_donors);
         about_us = view.findViewById(R.id.about_us);
         rate_us = view.findViewById(R.id.rate_us);
         image_slider = view.findViewById(R.id.image_slider);
+        first_row = view.findViewById(R.id.first_row);
+        sec_row = view.findViewById(R.id.sec_row);
+        third_row = view.findViewById(R.id.third_row);
+        reloadHome = view.findViewById(R.id.reloadHome);
+    }
 
-        ImageSlider();
+    private void Reload(){
+        reloadHome.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Init();
+                ImageSlider();
+                Anim();
+                reloadHome.setRefreshing(false);
+            }
+        });
+    }
 
+    private void OnClicks() {
         ins.setOnClickListener(v -> {
-           startActivity(new Intent(getContext(),Instructions.class));
+            startActivity(new Intent(getContext(),Instructions.class));
         });
 
         benefit.setOnClickListener(v -> {
@@ -91,121 +112,13 @@ public class HomeFragment extends Fragment {
         rate_us.setOnClickListener(v -> {
             rateUsOnGooglePlay();
         });
-
-
-        return view;
-
     }
 
-    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    boolean isVal = false;
-    private boolean getAdsFirebase() {
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference reference = database.getReference("admob");
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String settings = snapshot.child("settings").getValue(String.class);
-                String device_id = snapshot.child("device_id").getValue(String.class);
-                String banner = snapshot.child("banner").getValue(String.class);
-                String interstitials = snapshot.child("interstitials").getValue(String.class);
-                assert settings != null;
-                if (settings.contains("ON")){
-                    isVal = true;
-                    assert device_id != null;
-                    initAdmobAd(device_id);
-                    loadFullscreenAd(interstitials);
-
-                }else if (settings.contains("OFF")){
-                    isVal = false;
-                    Toast.makeText(getContext(), "ads No Coming!", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    isVal = false;
-                    Toast.makeText(getContext(), "Nothing!", Toast.LENGTH_SHORT).show();
-                }
-            }//onDataChange
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-
-        return isVal;
+    private void Anim() {
+        YoYo.with(Techniques.SlideInDown).delay(0).repeat(0).duration(1000).playOn(first_row);
+        YoYo.with(Techniques.SlideInLeft).delay(0).repeat(0).duration(1000).playOn(sec_row);
+        YoYo.with(Techniques.SlideInRight).delay(0).repeat(0).duration(1000).playOn(third_row);
     }
-    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    int BANNER_AD_CLICK_COUNT =0;
-    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    // loadFullscreenAd method starts here.....
-    InterstitialAd mInterstitialAd;
-    int FULLSCREEN_AD_LOAD_COUNT=0;
-    private void loadFullscreenAd(String url){
-
-        //Requesting for a fullscreen Ad
-        AdRequest adRequest = new AdRequest.Builder().build();
-        InterstitialAd.load(requireContext(),url, adRequest, new InterstitialAdLoadCallback() {
-            @Override
-            public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
-                // The mInterstitialAd reference will be null until
-                // an ad is loaded.
-                mInterstitialAd = interstitialAd;
-
-                //Fullscreen callback || Requesting again when an ad is shown already
-                mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback(){
-                    @Override
-                    public void onAdDismissedFullScreenContent() {
-                        // Called when fullscreen content is dismissed.
-                        //User dismissed the previous ad. So we are requesting a new ad here
-                        FULLSCREEN_AD_LOAD_COUNT++;
-                        loadFullscreenAd(url);
-                        Log.d("FULLSCREEN_AD_LOAD_COUNT", ""+FULLSCREEN_AD_LOAD_COUNT);
-
-                        if (targetActivity!=null) startActivity(targetActivity);
-
-                    }
-
-                }); // FullScreen Callback Ends here
-
-
-            }
-            @Override
-            public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
-                // Handle the error
-                mInterstitialAd = null;
-            }
-
-        });
-
-    }
-    // loadFullscreenAd method ENDS  here..... >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-    private void initAdmobAd(String id){
-
-        if (id.length()>12){
-            //Adding your device id -- to avoid invalid activity from your device
-            List<String> testDeviceIds = Collections.singletonList(id);
-            RequestConfiguration configuration =
-                    new RequestConfiguration.Builder().setTestDeviceIds(testDeviceIds).build();
-            MobileAds.setRequestConfiguration(configuration);
-        }
-
-        //Init Admob Ads
-        MobileAds.initialize(requireContext(), initializationStatus -> {
-            Toast.makeText(requireContext(), "coming", Toast.LENGTH_SHORT).show();
-        });
-
-
-    }
-    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
     private void ImageSlider(){
         final List<SlideModel> donationImages = new ArrayList<>();
@@ -225,13 +138,6 @@ public class HomeFragment extends Fragment {
                     }
                 });
     }
-
-    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-
-    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
     private void rateUsOnGooglePlay(){
 
